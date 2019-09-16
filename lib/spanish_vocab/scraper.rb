29e8cc955ef5c9c313b750133abd0ea29c4f933e
Laura Berge <lauraeberge@gmail.com>
2019-09-16 03:51:38 +0000

    added to scraper class

--- lib/spanish_vocab/scraper.rb
+++ lib/spanish_vocab/scraper.rb
@@ -4,5 +4,18 @@ require 'open-uri'
 module SpanishVocab
   class Scraper
     
+    def self.scrape(url)
+
+      doc = Nokogiri::HTML(open(url))
+      topic_array = []
+
+      doc.css(".content table a").each do |block|
+        topic = block.text
+        link = block.attribute("href").value
+        topic_array << {:name => topic, :link => link, :vocabulary => []}
+      end
+      topic_array
+    end
+    
   end
 end
--- spanish_vocab.gemspec
+++ spanish_vocab.gemspec
@@ -38,5 +38,5 @@ Gem::Specification.new do |spec|
 
   spec.add_development_dependency "bundler", "~> 2.0"
   spec.add_development_dependency "rake", "~> 10.0"
-  spec.add_development_dependency "nokogiri", "1.6"
+  spec.add_development_dependency "nokogiri", "~> 1.6.8"
 end

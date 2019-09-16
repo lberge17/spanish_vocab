30da86a41ce473149f030a547e00681d38ef4684
Laura Berge <lauraeberge@gmail.com>
2019-09-16 03:00:59 +0000

    first commit

---
+++ lib/spanish_vocab.rb
@@ -0,0 +1,6 @@
+require "spanish_vocab/version"
+
+module SpanishVocab
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/spanish_vocab/version.rb
@@ -0,0 +1,3 @@
+module SpanishVocab
+  VERSION = "0.1.0"
+end

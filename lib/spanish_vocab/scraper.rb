df8acca9fceb40ddf3356717789e3c0dd33033c2
Laura Berge <lauraeberge@gmail.com>
2019-09-16 03:57:48 +0000

    Adds to scraper.

--- lib/spanish_vocab/scraper.rb
+++ lib/spanish_vocab/scraper.rb
@@ -17,5 +17,19 @@ module SpanishVocab
       topic_array
     end
     
+    def self.scrape_vocab(url)
+      doc = Nokogiri::HTML(open(url))
+      vocab_array = []
+
+      doc.css("table:first tbody tr").each_with_index do |block, index|
+        if (block.css("td")[0]) != nil && (block.css("td")[1]) != nil
+          vocab_array << {:translation => block.css("td")[0].text,
+            :spanish => block.css("td")[1].text
+          }
+        end
+      end
+      vocab_array
+    end
+    
   end
 end

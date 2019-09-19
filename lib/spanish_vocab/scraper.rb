cecc933b95ca83fa566edbfb4f336026e2cfd12f
Laura Berge <laura@Lauras-MacBook-Pro-2.local>
2019-09-19 10:16:25 -0400

    Removed one of the topics that was formatted differently from the rest

--- lib/spanish_vocab/scraper.rb
+++ lib/spanish_vocab/scraper.rb
@@ -3,33 +3,32 @@ require 'open-uri'
 
 module SpanishVocab
   class Scraper
-    
+
     def self.scrape(url)
 
       doc = Nokogiri::HTML(open(url))
       topic_array = []
 
       doc.css(".content table a").each do |block|
-        topic = block.text
-        link = block.attribute("href").value
-        topic_array << {:name => topic, :link => link, :vocabulary => []}
+        if block.text != "Time, days, months"
+          topic = block.text
+          link = block.attribute("href").value
+          topic_array << {:name => topic, :link => link, :vocabulary => []}
+        end
       end
       topic_array
     end
-    
+
     def self.scrape_vocab(url)
       doc = Nokogiri::HTML(open(url))
-      vocab_array = []
 
-      doc.css("table:first tbody tr").each_with_index do |block, index|
-        if (block.css("td")[0]) != nil && (block.css("td")[1]) != nil
-          vocab_array << {:translation => block.css("td")[0].text,
+      doc.css("table:first tbody tr").map do |block|
+        {:translation => block.css("td")[0].text,
             :spanish => block.css("td")[1].text
           }
-        end
       end
-      vocab_array
+
     end
-    
+
   end
 end
--- lib/spanish_vocab/topic.rb
+++ lib/spanish_vocab/topic.rb
@@ -1,24 +1,27 @@
+require "pry"
+
+
 module SpanishVocab
   class Topic
     attr_accessor :name, :link, :vocabulary
     @@all = []
-    
+
     def initialize(topic_hash)
       topic_hash.each{|k, v| self.send(("#{k}="), v)}
       @@all << self
     end
-    
+
     def self.create_from_collection(topic_array)
       topic_array.each{|topic_hash| topic = SpanishVocab::Topic.new(topic_hash)}
     end
-    
+
     def add_vocabulary(vocab_array)
       vocab_array.each{|vocab_hash| @vocabulary << SpanishVocab::Vocab.new(vocab_hash)}
     end
-    
+
     def self.all
       @@all
     end
-    
+
   end
 end
